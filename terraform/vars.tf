a5a52447d3942548c28a8ab3fb053440fce60025
Alon Dotan <alon.dotan@takipi.com>
2017-12-11 11:08:49 +0200

    minor updates

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -30,7 +30,9 @@ variable "amis" {
   type = "map"
   default = {
     ubuntu_16_04 = "ami-aa2ea6d0"
+    ubuntu_14_04 = "ami-c29e1cb8"
     centos_7 = "ami-46c1b650"
+    centos_6 = "ami-01e2a017"
   }
 }
 
@@ -38,6 +40,8 @@ variable "user_names" {
   type = "map"
   default = {
     ubuntu_16_04 = "ubuntu"
+    ubuntu_14_04 = "ubuntu"
     centos_7 = "centos"
+    centos_6 = "centos"
   }
 }
